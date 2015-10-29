b9d72d355f383161add5cc7a05eb0d1f62e73caf
Vasily Golubev <w.golubev@mail.ru>
2015-10-29 07:42:32 -0400

    [khromova] update 3d.

--- khromova/model3d.py
+++ khromova/model3d.py
@@ -7,7 +7,6 @@ import struct
 
 # constants
 sources = ['Area_1_J2_1', 'Area_1_A3_2', 'Area_1_Aan_3', 'Area_1_Ahr_4', 'Area_1_Iu_P2_6', 'Area_1_P1AR_I_7', 'Area_1_P1AR_II_8', 'Area_1_P1S_III_9', 'Area_1_C2_10', 'Area_1_Acb_5']
-#sources = ['Area_1_J2_1', 'Area_1_A3_2']
 #! manual calculation
 x_min = 554400
 x_max = 558000
@@ -15,8 +14,8 @@ y_min = 7623300
 y_max = 7626700
 
 # common stuff
-nx = 100
-ny = 100
+nx = 200
+ny = 200
 xi = np.linspace(x_min, x_max, nx)
 yi = np.linspace(y_min, y_max, ny)
 
@@ -40,12 +39,25 @@ for source in sources:
 		h_prev = np.zeros(h.shape)
 
 	# create mesh
-	NX = nx - 1
-	NY = ny - 1
-	NZ = 10 # for each layers
+	NX = nx - 3
+	NY = ny - 3
+	if source == 'Area_1_P1S_III_9':
+		NZ = 40
+	elif source == 'Area_1_Iu_P2_6' :
+		NZ = 60
+	elif source == 'Area_1_J2_1':
+		NZ = 60
+	elif source == 'Area_1_Aan_3':
+		NZ = 30
+	elif source == 'Area_1_C2_10' or source == 'Area_1_Ahr_4' or source == 'Area_1_A3_2':
+		NZ = 10
+	else:
+		print "O!"
 	coords = np.zeros((NX, NY, NZ), dtype = ('float64', 3))
 	for i in range(NX):
 		for j in range(NY):
+			if math.isnan(h[i][j]):
+				print "NAN. Please, reload me!"
 			zi = np.linspace(h[i][j], h_prev[i][j], NZ)
 			for k in range(NZ):
 				coords[i, j, k][0] = xi[i] - x_min
